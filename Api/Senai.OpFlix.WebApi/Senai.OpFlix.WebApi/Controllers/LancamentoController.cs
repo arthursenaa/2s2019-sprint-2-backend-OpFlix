71a2aad870a7ccef32616278c23b40cdcae755e0
Arthur Senna de Andrade Souza <44761640804@info.local>
2019-09-13 17:13:36 -0300

    OpFlix

--- Api/Senai.OpFlix.WebApi/Senai.OpFlix.WebApi/Controllers/LancamentoController.cs
+++ Api/Senai.OpFlix.WebApi/Senai.OpFlix.WebApi/Controllers/LancamentoController.cs
@@ -18,6 +18,8 @@ namespace Senai.OpFlix.WebApi.Controllers
 
         LancamentoRepository LancamentoRepository = new LancamentoRepository();
 
+
+
         [Authorize]
         [HttpGet]
         public IActionResult Listar()
